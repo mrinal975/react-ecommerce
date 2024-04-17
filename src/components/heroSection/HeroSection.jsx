a67f8844ba957f8ec2c989ae19372b501cb2ec5f
Mrinal Mallik <mrinal@nagorik.tech>
2024-04-17 08:00:57 +0600

    hero section

---
+++ src/components/heroSection/HeroSection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HeroSection() {
+  return (
+    <>
+      <div>
+        <img className=" h-44 lg:h-full" src="../img/hero1.png" alt="" />
+      </div>
+    </>
+  );
+}
+
+export default HeroSection;
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Layout from "../../components/layout/Layout";
+import HeroSection from "../../components/heroSection/HeroSection";
 
 function HomePage() {
   return (
     <Layout>
-      <div>HomePage</div>
+      <HeroSection />
     </Layout>
   );
 }
