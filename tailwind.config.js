cde38698d2fb61065661785e3de4024e7d758608
Mrinal Mallik <mrinalmallik1@gmail.com>
2024-07-30 20:04:33 +0600

    asset added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,20 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-    container: {
-      padding: {
-        DEFAULT: "1rem",
-        sm: "2rem",
-        lg: "4rem",
-        xl: "5rem",
-        "2xl": "6rem",
+    extend: {
+      colors: {
+        primary: "#fea928",
+        secondary: "#ed8900",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
       },
     },
   },
