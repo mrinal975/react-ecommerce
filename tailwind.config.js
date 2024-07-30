dcb2fa4d3ca5a79f1998c2bcca6640ec0a8b0933
Mrinal Mallik <mrinalmallik1@gmail.com>
2024-07-30 19:53:33 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+  },
+  plugins: [],
+};
