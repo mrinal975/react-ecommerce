aca56bb6001809463345d77a8ad105e8655c550e
Mrinal Mallik <mrinalmallik1@gmail.com>
2024-07-31 17:52:23 +0600

    animation on scroll package install and initialize

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,20 @@ import DefaultLayout from "@/pages/layouts/DefaultLayout";
 import Routes from "@/Routes/Index";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
   return (
     <DefaultLayout>
       <Routes />
