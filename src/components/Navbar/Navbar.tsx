a6e4d509c3d8f1476bcc54ea859f642154feaf7f
Mrinal Mallik <mrinalmallik1@gmail.com>
2024-07-31 06:42:21 +0600

    srach icon

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import { IoMdSearch } from "react-icons/io";
+
 function Navbar() {
   return (
     <div
@@ -13,12 +15,16 @@ function Navbar() {
               <p className=" font-bold text-xl">Shopsy</p>
             </div>
             <div className="">
-              <div className="group">
+              <div className="relative group hidden sm:block">
                 <input
                   type="text"
                   className="w-[150px] sm:w-[150px] group-hover:w-[250px] transition-all duration-300
-               border border-gray-300 rounded-full px-2 focus:outline-none focus:border-1 focus:border-orange-400"
+               border border-gray-300 rounded-full px-2 focus:outline-none focus:border-1 focus:border-primary"
                   placeholder="search"
+                />
+                <IoMdSearch
+                  className="absolute top-1/2 -translate-y-1/2 right-1 text-gray-500 group-hover:text-primary
+                               transition-all duration-300"
                 />
               </div>
               <div className=""></div>
