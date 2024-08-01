83808baf69daa3a046e7c77fbbeda80e5dac5e7f
Mrinal Mallik <mrinalmallik1@gmail.com>
2024-08-01 12:47:20 +0600

    subscribe manu

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,7 @@
 import Banner from "@/components/Banner/Banner";
 import Hero from "@/components/Hero/Hero";
 import Products from "@/components/Products/Products";
+import Subscribe from "@/components/Subscribe/Subscribe";
 import TopProducts from "@/components/TopProducts/TopProducts";
 function home() {
   return (
@@ -9,6 +10,7 @@ function home() {
       <Products />
       <TopProducts />
       <Banner />
+      <Subscribe />
     </>
   );
 }
