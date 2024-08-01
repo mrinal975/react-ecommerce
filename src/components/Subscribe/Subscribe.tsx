77de186e3c4fc78d76784da68f37b9913b1f7500
Mrinal Mallik <mrinalmallik1@gmail.com>
2024-08-01 13:01:35 +0600

    subscription

--- src/components/Subscribe/Subscribe.tsx
+++ src/components/Subscribe/Subscribe.tsx
@@ -1,7 +1,34 @@
 import React from "react";
-
+import Banner from "../../assets/website/orange-pattern.jpg";
+const BannerImg = {
+  backgroundImage: `url(${Banner})`,
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  height: "100%",
+  width: "100%",
+};
 function Subscribe() {
-  return <div>Subscribe</div>;
+  return (
+    <div
+      data-aos="zoom-in"
+      className="mb-20 bg-gray-100 dark:bg-gray-800 text-white"
+      style={BannerImg}
+    >
+      <div className="container backdrop-blur-sm py-10">
+        <div className="space-y-6 max-w-xl mx-auto">
+          <h1 className="text-2xl !text-center sm:text-left sm:text-4xl font-semibold">
+            Get Notified About New Products
+          </h1>
+          <input
+            type="text"
+            className="w-full p-3"
+            placeholder="Enter your email"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Subscribe;
